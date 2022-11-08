6bfe7f937b68238a71055d478ea2150a37047dbf
KaMaksimKa <karagodin.maks05@mail.ru>
2022-11-08 14:38:30 +0300

    Добавлена CreatedDate в PublicationModel

--- Desgram.Api/Models/PublicationModel.cs
+++ Desgram.Api/Models/PublicationModel.cs
@@ -7,6 +7,7 @@
         public string Description { get; set; } = null!;
         public int AmountLikes { get; set; }
         public int AmountComments { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
         public List<PublicationContentModel> ContentModels { get; set; } = null!;
         public List<string> HashTags { get; set; } = null!;
     }
